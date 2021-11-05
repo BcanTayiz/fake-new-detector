936c88206d9ae0b66760ef1e121cd56f6fa93dae
Barış Can <brscntyz@gmail.com>
2021-11-05 13:04:54 +0300

    last

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ const results = [];
 
 
 
-// create the network 
-var classifier = new natural.BayesClassifier();
+
 
 if (fs.existsSync('./classifier.json')) {
+    
     console.log("Classifier is loaded")
     natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
         
-        let category = classifier.classify("Tigers seem innocent but the cat was very strong with its motivation and the technique and its learned martial arts. Cat used its knife and danger cries to destroy its enemy in a war battle that the tiger even couldn't resist")
-    
-        console.log("Trained ML Neural Network")
-        console.log(category)
-
-        function checkFakeNews(string){
+        function classifierML(string){
             let result = classifier.classify(string)
             return result
         }
-        module.exports = checkFakeNews
+
+        let result = classifierML(string)
+        module.exports = classifierML
     }); 
 }
+
+    
+
+
 
 
 
