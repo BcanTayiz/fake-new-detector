ea3ceb712a14b40b39f54864597d6dc65d3362d0
Barış Can <brscntyz@gmail.com>
2021-11-05 11:57:14 +0300

    code cleaned

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const results = [];
 // create the network 
 var classifier = new natural.BayesClassifier();
 
-let addData = true
 if (fs.existsSync('./classifier.json')) {
     console.log("Classifier is loaded")
     natural.BayesClassifier.load('classifier.json', null, function(err, classifier) {
@@ -25,49 +24,7 @@ if (fs.existsSync('./classifier.json')) {
         }
         module.exports = checkFakeNews
     }); 
-}else{
-   console.log('err')
-   fs.createReadStream('train.csv')
-   .pipe(csv())
-   .on('data', (data) => results.push(data))
-   .on('end', () => {
-     console.log('data installed and apply model is starting')
-     applyModel()
-     // [
-     //   { NAME: 'Daffy Duck', AGE: '24' },
-     //   { NAME: 'Bugs Bunny', AGE: '22' }
-     // ]
-   });
 }
-
-
-async function applyModel(){
-    console.log('applyModel started')
-    
-    results.forEach(item => {
-    
-        classifier.addDocument(String(item['text'].split('\n').join(' ')),Number(item['label']))
-    })
-    console.log('network starts to train')
-    classifier.train();
-    // Save network state to JSON file.
-    console.log(classifier.classify('i am short silver'),"classification result")
-    classifier.save('classifier.json', function(err, classifier) {
-        // the classifier is saved to the classifier.json file!
-        if(err){
-            console.log(err)
-        }
-    });
-
-    // see the result from trained network
-    let category = classifier.classify("Tigers seem innocent but the cat was very strong with its motivation and the technique and its learned martial arts. Cat used its knife and danger cries to destroy its enemy in a war battle that the tiger even couldn't resist")
-
-    console.log("New Trained Neural Network")
-    console.log(category)
-}
-
-
-
 
 
 
